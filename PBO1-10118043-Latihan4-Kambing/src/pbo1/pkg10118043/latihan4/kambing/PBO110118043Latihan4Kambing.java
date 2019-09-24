7269b955f6546393aaebbabffd4f90b43e343d29
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 15:08:06 +0700

    Add files via upload
    PBO1 = IF-01

---
+++ PBO1-10118043-Latihan4-Kambing/src/pbo1/pkg10118043/latihan4/kambing/PBO110118043Latihan4Kambing.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118043.latihan4.kambing;
+
+/**
+ *
+ * @author user
+ */
+public class PBO110118043Latihan4Kambing {
+    
+    public void tk(){
+        int jk = 0;
+        
+        jk = jk + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " +jk);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBO110118043Latihan4Kambing kj = new PBO110118043Latihan4Kambing();
+        kj.tk();
+    }
+    
+}
